ac0bd9f534518665e681021973942701eda58e92
Risky Irfansyah <55643683+RiskyIrfansyah@users.noreply.github.com>
2019-10-27 10:51:37 +0700

    Add files via upload

---
+++ PBO2-10118085-Latihan44-HukumOhm/src/pbo2/pkg10118085/latihan44/hukumohm/PBO210118085Latihan44HukumOhm.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118085.latihan44.hukumohm;
+
+/**
+ *
+ * @author
+ * NAMA     : Risky Irfansyah
+ * KELAS    : IF-2
+ * NIM      : 10118085
+ * Deskripsi Program : Membuat program berbasis object untuk menghitung tegangan
+ * dengan hukum ohm
+ */
+public class PBO210118085Latihan44HukumOhm {
+
+     public static void main(String[] args) {
+        new Baterai();
+        Baterai baterai = new Baterai(3, 12);
+    }
+    
+}
